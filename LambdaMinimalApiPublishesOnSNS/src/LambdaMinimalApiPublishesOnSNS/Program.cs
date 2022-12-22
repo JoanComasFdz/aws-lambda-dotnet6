71589632c6e3055374d08efed0c6ed383f530379
Joan Comas <joan.comas.fdz@gmail.com>
2022-12-22 17:18:12 +0100

    Reverted lambda publihes to sns to previous xray and services code

--- LambdaMinimalApiPublishesOnSNS/src/LambdaMinimalApiPublishesOnSNS/Program.cs
+++ LambdaMinimalApiPublishesOnSNS/src/LambdaMinimalApiPublishesOnSNS/Program.cs
@@ -9,9 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+AWSSDKHandler.RegisterXRayForAllServices();
+
 // Add AWS Lambda support. When application is run in Lambda Kestrel is swapped out as the web server with Amazon.Lambda.AspNetCoreServer. This
 // package will act as the webserver translating request and responses between the Lambda event source and ASP.NET Core.
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
+
+var snsTopicArn = Environment.GetEnvironmentVariable("AWS_SNS_TOPICARN");
+var currentAwsRegion = Environment.GetEnvironmentVariable("AWS_REGION");
+var regionEndpoint = RegionEndpoint.GetBySystemName(currentAwsRegion);
+builder.Services.AddScoped(_ => new AmazonSimpleNotificationServiceClient(regionEndpoint));
+builder.Services.AddScoped<ISnsPublisher>(serviceProvider => new SnsPublisher(serviceProvider.GetService<AmazonSimpleNotificationServiceClient>(), snsTopicArn));
 
 var app = builder.Build();
 
@@ -23,18 +31,11 @@ app.MapGet("/", () => "Welcome to running ASP.NET Core Minimal API on AWS Lambda
 
 static async Task<IResult> AddUser(
     [FromBody] User user,
-    [FromServices] ILogger<Program> logger
-    //, [FromServices] ISnsPublisher publisher
+    [FromServices] ILogger<Program> logger,
+    [FromServices] ISnsPublisher snsPublisher
     )
 {
     logger.LogInformation("The user will be added: {user}", user);
-
-    AWSSDKHandler.RegisterXRayForAllServices();
-
-    var snsTopicArn = Environment.GetEnvironmentVariable("AWS_SNS_TOPICARN");
-    var currentAwsRegion = Environment.GetEnvironmentVariable("AWS_REGION");
-    var regionEndpoint = RegionEndpoint.GetBySystemName(currentAwsRegion);
-    var snsPublisher = new SnsPublisher(new AmazonSimpleNotificationServiceClient(regionEndpoint), snsTopicArn);
 
     await snsPublisher.PublishAsync(new CreateUserCommand(user));
     
